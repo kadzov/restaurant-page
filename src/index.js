e1f3c52c1c1433bca44ab29193a08cb43c09f02e
Marat Kadzov <mtkadzov@gmail.com>
2021-07-31 19:37:46 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 import './style.css';
-
 import logo from './images/logo.jpg';
 import pizzeria from './images/pizzeria.jpg';
-import capricciosa from './images/pizza/capricciosa.jpg';
-import hawaiian from './images/pizza/hawaiian.jpg';
-import margherita from './images/pizza/margherita.jpg';
-import neapolitan from './images/pizza/neapolitan.jpg';
-import pepperoni from './images/pizza/pepperoni.jpg';
-import quattroFormaggi from './images/pizza/quattro-formaggi.jpg';
 
 (() => {
   const img = new Image();
   img.src = logo;
-  img.id = 'logo'
+  img.id = 'logo';
   content.appendChild(img);
   const img2 = new Image();
   img2.src = pizzeria;
-  img2.id = 'pizzeria'
+  img2.id = 'pizzeria';
   content.appendChild(img2);
   const header = document.createElement('h1');
   header.textContent = 'THE BEST NEW YORK STYLE PIZZA AROUND!';
@@ -26,67 +19,4 @@ import quattroFormaggi from './images/pizza/quattro-formaggi.jpg';
   content.appendChild(description);
 })();
 
-const assortment = document.createElement('div');
-assortment.id = 'assortment';
-content.appendChild(assortment);
-
-(() => {
-  const container = document.createElement('div');
-  assortment.appendChild(container);
-  const name = document.createElement('h2');
-  name.textContent = 'Capricciosa';
-  container.appendChild(name);
-  const img = new Image();
-  img.src = capricciosa;
-  container.appendChild(img);
-})();
-(() => {
-  const container = document.createElement('div');
-  assortment.appendChild(container);
-  const name = document.createElement('h2');
-  name.textContent = 'Hawaiian';
-  container.appendChild(name);
-  const img = new Image();
-  img.src = hawaiian;
-  container.appendChild(img);
-})();
-(() => {
-  const container = document.createElement('div');
-  assortment.appendChild(container);
-  const name = document.createElement('h2');
-  name.textContent = 'Margherita';
-  container.appendChild(name);
-  const img = new Image();
-  img.src = margherita;
-  container.appendChild(img);
-})();
-(() => {
-  const container = document.createElement('div');
-  assortment.appendChild(container);
-  const name = document.createElement('h2');
-  name.textContent = 'Neapolitan';
-  container.appendChild(name);
-  const img = new Image();
-  img.src = neapolitan;
-  container.appendChild(img);
-})();
-(() => {
-  const container = document.createElement('div');
-  assortment.appendChild(container);
-  const name = document.createElement('h2');
-  name.textContent = 'Pepperoni';
-  container.appendChild(name);
-  const img = new Image();
-  img.src = pepperoni;
-  container.appendChild(img);
-})();
-(() => {
-  const container = document.createElement('div');
-  assortment.appendChild(container);
-  const name = document.createElement('h2');
-  name.textContent = 'Quattro Formaggi';
-  container.appendChild(name);
-  const img = new Image();
-  img.src = quattroFormaggi;
-  container.appendChild(img);
-})();
+//check other projects from odin and ffc for grid
---
+++ src/menu.js
@@ -0,0 +1,72 @@
+import './style.css';
+import capricciosa from './images/pizza/capricciosa.jpg';
+import hawaiian from './images/pizza/hawaiian.jpg';
+import margherita from './images/pizza/margherita.jpg';
+import neapolitan from './images/pizza/neapolitan.jpg';
+import pepperoni from './images/pizza/pepperoni.jpg';
+import quattroFormaggi from './images/pizza/quattro-formaggi.jpg';
+
+const assortment = document.createElement('div');
+assortment.id = 'assortment';
+content.appendChild(assortment);
+
+(() => {
+  const container = document.createElement('div');
+  assortment.appendChild(container);
+  const name = document.createElement('h2');
+  name.textContent = 'Capricciosa';
+  container.appendChild(name);
+  const img = new Image();
+  img.src = capricciosa;
+  container.appendChild(img);
+})();
+(() => {
+  const container = document.createElement('div');
+  assortment.appendChild(container);
+  const name = document.createElement('h2');
+  name.textContent = 'Hawaiian';
+  container.appendChild(name);
+  const img = new Image();
+  img.src = hawaiian;
+  container.appendChild(img);
+})();
+(() => {
+  const container = document.createElement('div');
+  assortment.appendChild(container);
+  const name = document.createElement('h2');
+  name.textContent = 'Margherita';
+  container.appendChild(name);
+  const img = new Image();
+  img.src = margherita;
+  container.appendChild(img);
+})();
+(() => {
+  const container = document.createElement('div');
+  assortment.appendChild(container);
+  const name = document.createElement('h2');
+  name.textContent = 'Neapolitan';
+  container.appendChild(name);
+  const img = new Image();
+  img.src = neapolitan;
+  container.appendChild(img);
+})();
+(() => {
+  const container = document.createElement('div');
+  assortment.appendChild(container);
+  const name = document.createElement('h2');
+  name.textContent = 'Pepperoni';
+  container.appendChild(name);
+  const img = new Image();
+  img.src = pepperoni;
+  container.appendChild(img);
+})();
+(() => {
+  const container = document.createElement('div');
+  assortment.appendChild(container);
+  const name = document.createElement('h2');
+  name.textContent = 'Quattro Formaggi';
+  container.appendChild(name);
+  const img = new Image();
+  img.src = quattroFormaggi;
+  container.appendChild(img);
+})();
