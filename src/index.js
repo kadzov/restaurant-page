e9adac29b87925662cea83dececd9e540256161c
Marat Kadzov <mtkadzov@gmail.com>
2021-07-31 22:38:10 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
 import './style.css';
 import logo from './images/logo.jpg';
 import pizzeria from './images/pizzeria.jpg';
-
 (() => {
   const img = new Image();
   img.src = logo;
   img.id = 'logo';
   content.appendChild(img);
-  const img2 = new Image();
-  img2.src = pizzeria;
-  img2.id = 'pizzeria';
-  content.appendChild(img2);
-  const header = document.createElement('h1');
-  header.textContent = 'THE BEST NEW YORK STYLE PIZZA AROUND!';
-  content.appendChild(header);
-  const description = document.createElement('p');
-  description.textContent = 'The Russo family has been serving up the best New York Style Pizza and authentic Italian cuisine for years. Our recipes have been passed down from generation to generation all the way from Naples and Sicily. Russo’s mission is to serve New York style pizza and authentic Italian food worldwide so that no matter your location, you’ll be able to enjoy the delicious, traditional Russo family recipes.';
-  content.appendChild(description);
 })();
+(() => {
+  const nav = document.createElement('nav');
 
-//check other projects from odin and ffc for grid
+  const home = document.createElement('a');
+  home.textContent = 'HOME';
+  nav.appendChild(home);
+
+  const menu = document.createElement('a');
+  menu.textContent = 'MENU';
+  nav.appendChild(menu);
+
+  const location = document.createElement('a');
+  location.textContent = 'LOCATION';
+  nav.appendChild(location);
+
+  content.appendChild(nav);
+})();
+(() => {
+  const img = new Image();
+  img.src = pizzeria;
+  img.id = 'pizzeria';
+  content.appendChild(img);
+})();
+(() => {
+  const h1 = document.createElement('h1');
+  h1.textContent = 'THE BEST NEW YORK STYLE PIZZA AROUND!';
+  content.appendChild(h1);
+})();
+(() => {
+  const p = document.createElement('p');
+  p.textContent = 'The Russo family has been serving up the best New York Style Pizza and authentic Italian cuisine for years. Our recipes have been passed down from generation to generation all the way from Naples and Sicily. Russo’s mission is to serve New York style pizza and authentic Italian food worldwide so that no matter your location, you’ll be able to enjoy the delicious, traditional Russo family recipes.';
+  content.appendChild(p);
+})();
--- src/menu.js
+++ src/menu.js
@@ -6,6 +6,10 @@ import neapolitan from './images/pizza/neapolitan.jpg';
 import pepperoni from './images/pizza/pepperoni.jpg';
 import quattroFormaggi from './images/pizza/quattro-formaggi.jpg';
 
+//check other projects from odin and ffc for grid
+//name variables as html elements
+//divide elements on functions
+
 const assortment = document.createElement('div');
 assortment.id = 'assortment';
 content.appendChild(assortment);
@@ -13,9 +17,11 @@ content.appendChild(assortment);
 (() => {
   const container = document.createElement('div');
   assortment.appendChild(container);
+
   const name = document.createElement('h2');
   name.textContent = 'Capricciosa';
   container.appendChild(name);
+
   const img = new Image();
   img.src = capricciosa;
   container.appendChild(img);
@@ -23,9 +29,11 @@ content.appendChild(assortment);
 (() => {
   const container = document.createElement('div');
   assortment.appendChild(container);
+
   const name = document.createElement('h2');
   name.textContent = 'Hawaiian';
   container.appendChild(name);
+
   const img = new Image();
   img.src = hawaiian;
   container.appendChild(img);
@@ -33,9 +41,11 @@ content.appendChild(assortment);
 (() => {
   const container = document.createElement('div');
   assortment.appendChild(container);
+
   const name = document.createElement('h2');
   name.textContent = 'Margherita';
   container.appendChild(name);
+
   const img = new Image();
   img.src = margherita;
   container.appendChild(img);
@@ -43,9 +53,11 @@ content.appendChild(assortment);
 (() => {
   const container = document.createElement('div');
   assortment.appendChild(container);
+
   const name = document.createElement('h2');
   name.textContent = 'Neapolitan';
   container.appendChild(name);
+
   const img = new Image();
   img.src = neapolitan;
   container.appendChild(img);
@@ -53,9 +65,11 @@ content.appendChild(assortment);
 (() => {
   const container = document.createElement('div');
   assortment.appendChild(container);
+
   const name = document.createElement('h2');
   name.textContent = 'Pepperoni';
   container.appendChild(name);
+
   const img = new Image();
   img.src = pepperoni;
   container.appendChild(img);
@@ -63,9 +77,11 @@ content.appendChild(assortment);
 (() => {
   const container = document.createElement('div');
   assortment.appendChild(container);
+
   const name = document.createElement('h2');
   name.textContent = 'Quattro Formaggi';
   container.appendChild(name);
+
   const img = new Image();
   img.src = quattroFormaggi;
   container.appendChild(img);
