8c938ff407b7d551cf0e8d2cb7639b000f490eed
Marat Kadzov <mtkadzov@gmail.com>
2021-08-16 20:56:09 +0300

    add

--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: { clean: true },
-  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  plugins: [new HtmlWebpackPlugin()],
   module: { rules: [{ test: /\.jpg$/, type: 'asset/resource' }] }
 };
+//{ template: './src/index.html' }
