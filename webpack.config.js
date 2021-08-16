8f27027ea008ead4a501492c21bbfac50b870ffd
Marat Kadzov <mtkadzov@gmail.com>
2021-08-17 02:29:23 +0300

    add

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const config = {
+  devServer: { hot: true },
   output: { clean: true },
   plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
   module: {
@@ -13,15 +14,12 @@ const config = {
 };
 
 module.exports = (env, argv) => {
-  if (argv.mode === 'development') {
-    config.devServer = { hot: true };
-  }
   if (argv.mode === 'production') {
+    config.optimization = { splitChunks: { chunks: 'all' } };
     config.output.filename = '[contenthash].js';
     config.plugins.push
       (new MiniCssExtractPlugin({ filename: '[contenthash].css' }));
     config.module.rules[0].use[0] = MiniCssExtractPlugin.loader;
-    config.optimization = { splitChunks: { chunks: 'all' } };
   }
   return config;
 };
