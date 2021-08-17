aabf10e7f59299690fc654169d488a64b45c2a05
Marat Kadzov <mtkadzov@gmail.com>
2021-08-17 12:38:16 +0300

    add

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const config = {
-  devServer: { hot: true },
   output: { clean: true },
   plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
   module: {
     rules: [
       { test: /\.scss/, use: ['style-loader', 'css-loader', 'sass-loader'] },
-      { test: /\.jpg/, type: 'asset/resource' },
+      { test: /\.jpg/, type: 'asset/resource' }
     ]
   }
 };
 
 module.exports = (env, argv) => {
-  if (argv.mode === 'production') {
+  if (argv.mode === 'development') {
+    devServer = { hot: true };
+  } else if (argv.mode === 'production') {
     config.optimization = { splitChunks: { chunks: 'all' } };
     config.output.filename = '[contenthash].js';
     config.plugins.push
       (new MiniCssExtractPlugin({ filename: '[contenthash].css' }));
     config.module.rules[0].use[0] = MiniCssExtractPlugin.loader;
-  }
-  return config;
+  } return config;
 };
