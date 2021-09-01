ca20f9d2b5e77a6ea15942c5468c1df384edb61f
Marat Kadzov <mtkadzov@gmail.com>
2021-09-01 16:46:55 +0300

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const config = {
+module.exports = env => ({
+  mode: env.production ? 'production' : 'development',
   entry: './src/index.ts',
-  output: { clean: true },
-  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  output: {
+    filename: env.production ? '[contenthash].js' : '[name].js',
+    clean: true
+  },
+  devServer: { hot: true },
+  optimization: env.production ? { splitChunks: { chunks: 'all' } } : {},
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new MiniCssExtractPlugin({ filename: '[contenthash].css' })
+  ],
   module: {
     rules: [
       { test: /\.jpg/, type: 'asset/resource' },
-      { test: /\.scss/, use: ['css-loader', 'sass-loader'] },
-      { test: /\.ts/, use: 'ts-loader' }
+      { test: /\.ts/, use: 'ts-loader' },
+      {
+        test: /\.scss/,
+        use: [
+          env.production
+            ? MiniCssExtractPlugin.loader
+            : 'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
     ]
   }
-};
-
-module.exports = (env, argv) => {
-  if (argv.mode === 'development') {
-    config.devServer = { hot: true };
-    config.module.rules[1].use.unshift('style-loader');
-  } else {
-    config.optimization = { splitChunks: { chunks: 'all' } };
-    config.output.filename = '[contenthash].js';
-    config.plugins.push
-      (new MiniCssExtractPlugin({ filename: '[contenthash].css' }));
-    config.module.rules[1].use.unshift(MiniCssExtractPlugin.loader);
-  } return config;
-};
+});
