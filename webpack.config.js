f79b1cc8b58a20221fa28bd19a1d6c70d95af839
Marat Kadzov <mtkadzov@gmail.com>
2021-09-01 21:07:09 +0300

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = env => ({
-  mode: env.production ? 'production' : 'development',
+const config = {
+  mode: 'development',
   entry: './src/index.ts',
-  output: {
-    filename: env.production ? '[contenthash].js' : '[name].js',
-    clean: true
-  },
+  output: { filename: '[name].js', clean: true },
   devServer: { hot: true },
-  optimization: env.production ? { splitChunks: { chunks: 'all' } } : {},
   plugins: [
     new HtmlWebpackPlugin({ template: './src/index.html' }),
     new MiniCssExtractPlugin({ filename: '[contenthash].css' })
@@ -17,17 +13,18 @@ module.exports = env => ({
   module: {
     rules: [
       { test: /\.jpg/, type: 'asset/resource' },
-      { test: /\.ts/, use: 'ts-loader' },
-      {
-        test: /\.scss/,
-        use: [
-          env.production
-            ? MiniCssExtractPlugin.loader
-            : 'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
+      { test: /\.scss/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.ts/, use: 'ts-loader' }
     ]
   }
-});
+};
+
+module.exports = env => {
+  if (env.production) {
+    config.mode = 'production';
+    config.output.filename = '[contenthash].js';
+    config.optimization = { splitChunks: { chunks: 'all' } };
+    config.module.rules[1].use[0] = MiniCssExtractPlugin.loader;
+  }
+  return config
+};
