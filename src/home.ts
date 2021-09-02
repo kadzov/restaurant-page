de4a5ff7a59193f0dcf7376520b5dc5769cea8f3
Marat Kadzov <mtkadzov@gmail.com>
2021-09-02 15:56:12 +0300

    add

--- src/home.ts
+++ src/home.ts
@@ -1,21 +1,22 @@
 import './home.scss';
 import pizzeria from './images/pizzeria.jpg';
+export { home };
 
-(() => {
+const home = () => {
+  const div = document.createElement('div');
+
   const img = new Image();
   img.src = pizzeria;
   img.id = 'pizzeria';
-  document.querySelector('#content').appendChild(img);
-})();
+  div.appendChild(img);
 
-(() => {
   const h1 = document.createElement('h1');
   h1.textContent = 'THE BEST NEW YORK STYLE PIZZA AROUND!';
-  document.querySelector('#content').appendChild(h1);
-})();
+  div.appendChild(h1);
 
-(() => {
   const p = document.createElement('p');
   p.textContent = 'The Russo family has been serving up the best New York Style Pizza and authentic Italian cuisine for years. Our recipes have been passed down from generation to generation all the way from Naples and Sicily. Russo’s mission is to serve New York style pizza and authentic Italian food worldwide so that no matter your location, you’ll be able to enjoy the delicious, traditional Russo family recipes.';
-  document.querySelector('#content').appendChild(p);
-})();
+  div.appendChild(p);
+
+  return div;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
 import './index.scss';
 import logo from './images/logo.jpg';
+import { home } from './home';
 
 (() => {
+  const div = document.createElement('div');
+
   const img = new Image();
   img.src = logo;
   img.id = 'logo';
-  document.querySelector('#content').appendChild(img);
-})();
+  div.appendChild(img);
 
-(() => {
   const nav = document.createElement('nav');
-
   const home = document.createElement('a');
   home.textContent = 'HOME';
   nav.appendChild(home);
-
   const menu = document.createElement('a');
   menu.textContent = 'MENU';
   nav.appendChild(menu);
-
   const location = document.createElement('a');
   location.textContent = 'LOCATION';
   nav.appendChild(location);
+  div.appendChild(nav);
 
-  document.querySelector('#content').appendChild(nav);
+  document.querySelector('#content').appendChild(div);
 })();
+
+document.querySelector('#content').appendChild(home());
