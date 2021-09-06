cd77023439658642f673b4dcce22dff4745e00ea
Marat Kadzov <mtkadzov@gmail.com>
2021-09-06 18:25:30 +0300

    add

--- src/home.ts
+++ src/home.ts
@@ -15,7 +15,8 @@ export default () => {
   div.appendChild(h1);
 
   const p = document.createElement('p');
-  p.textContent = 'The Russo family has been serving up the best New York Style Pizza and authentic Italian cuisine for years. Our recipes have been passed down from generation to generation all the way from Naples and Sicily. Russo’s mission is to serve New York style pizza and authentic Italian food worldwide so that no matter your location, you’ll be able to enjoy the delicious, traditional Russo family recipes.';
+  p.textContent =
+    'The Russo family has been serving up the best New York Style Pizza and authentic Italian cuisine for years. Our recipes have been passed down from generation to generation all the way from Naples and Sicily. Russo’s mission is to serve New York style pizza and authentic Italian food worldwide so that no matter your location, you’ll be able to enjoy the delicious, traditional Russo family recipes.';
   div.appendChild(p);
 
   return div;
