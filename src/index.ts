e3db529292540e7882c9f4b8328496c60a498a52
Marat Kadzov <mtkadzov@gmail.com>
2021-09-07 21:16:22 +0300

    add

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import './index.scss';
+
 import header from './header';
 // import home from './home';
-import menu from './menu';
+// import menu from './menu';
 // import location from './location';
 
 const content = document.querySelector('#content');
 
 content.appendChild(header());
 // content.appendChild(home());
-content.appendChild(menu());
+// content.appendChild(menu());
 // content.appendChild(location());
