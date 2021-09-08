ddc50d38afd9b7748d608f2c0cadfc5d7d20b658
Marat Kadzov <mtkadzov@gmail.com>
2021-09-08 18:16:39 +0300

    add

--- src/menu.ts
+++ src/menu.ts
@@ -11,118 +11,118 @@ export default () => {
   div.id = 'menu';
 
   const capricciosa = () => {
-    const section = document.createElement('section');
+    const article = document.createElement('article');
 
     const img = new Image();
     img.src = Capricciosa;
-    section.appendChild(img);
+    article.appendChild(img);
 
     const h2 = document.createElement('h2');
     h2.textContent = 'Capricciosa';
-    section.appendChild(h2);
+    article.appendChild(h2);
 
     const p = document.createElement('p');
     p.textContent =
       'Crumbled Italian sausage, pepperoni, Canadian bacon, beef, and Wisconsin mozzarella.';
-    section.appendChild(p);
+    article.appendChild(p);
 
-    return section;
+    return article;
   };
   div.appendChild(capricciosa());
 
   const hawaiian = () => {
-    const section = document.createElement('section');
+    const article = document.createElement('article');
 
     const img = new Image();
     img.src = Hawaiian;
-    section.appendChild(img);
+    article.appendChild(img);
 
     const h2 = document.createElement('h2');
     h2.textContent = 'Hawaiian';
-    section.appendChild(h2);
+    article.appendChild(h2);
 
     const p = document.createElement('p');
     p.textContent =
       'Fresh spinach, Roma tomatoes, feta cheese, Wisconsin mozzarella, black olives, and Sicilian extra-virgin olive oil garlic sauce.';
-    section.appendChild(p);
+    article.appendChild(p);
 
-    return section;
+    return article;
   };
   div.appendChild(hawaiian());
   const margherita = () => {
-    const section = document.createElement('section');
+    const article = document.createElement('article');
 
     const img = new Image();
     img.src = Margherita;
-    section.appendChild(img);
+    article.appendChild(img);
 
     const h2 = document.createElement('h2');
     h2.textContent = 'Margherita';
-    section.appendChild(h2);
+    article.appendChild(h2);
 
     const p = document.createElement('p');
     p.textContent =
       'Truffle olive oil sauce, Wisconsin mozzarella, Prosciutto di Parma, fresh arugula, and fresh mozzarella.';
-    section.appendChild(p);
+    article.appendChild(p);
 
-    return section;
+    return article;
   };
   div.appendChild(margherita());
   const neapolitan = () => {
-    const section = document.createElement('section');
+    const article = document.createElement('article');
 
     const img = new Image();
     img.src = Neapolitan;
-    section.appendChild(img);
+    article.appendChild(img);
 
     const h2 = document.createElement('h2');
     h2.textContent = 'Neapolitan';
-    section.appendChild(h2);
+    article.appendChild(h2);
 
     const p = document.createElement('p');
     p.textContent =
       'Pepperoni, sliced sausage, roasted peppers, red onions, Pecorino Romano cheese, Sicilian extra-virgin olive oil, and fresh garlic.';
-    section.appendChild(p);
+    article.appendChild(p);
 
-    return section;
+    return article;
   };
   div.appendChild(neapolitan());
   const pepperoni = () => {
-    const section = document.createElement('section');
+    const article = document.createElement('article');
 
     const img = new Image();
     img.src = Pepperoni;
-    section.appendChild(img);
+    article.appendChild(img);
 
     const h2 = document.createElement('h2');
     h2.textContent = 'Pepperoni';
-    section.appendChild(h2);
+    article.appendChild(h2);
 
     const p = document.createElement('p');
     p.textContent =
       'Prosciutto di Parma, arugula, basil, fig spread, Sicilian extravirgin olive oil, Wisconsin mozzarella, and balsamic glaze.';
-    section.appendChild(p);
+    article.appendChild(p);
 
-    return section;
+    return article;
   };
   div.appendChild(pepperoni());
   const quattroFormaggi = () => {
-    const section = document.createElement('section');
+    const article = document.createElement('article');
 
     const img = new Image();
     img.src = QuattroFormaggi;
-    section.appendChild(img);
+    article.appendChild(img);
 
     const h2 = document.createElement('h2');
     h2.textContent = 'Quattro Formaggi';
-    section.appendChild(h2);
+    article.appendChild(h2);
 
     const p = document.createElement('p');
     p.textContent =
       'House-made fennel sausage, jalapeño, roasted red peppers, garlic sauce, carmelized onions, goat cheese, and Wisconsin mozzarella.';
-    section.appendChild(p);
+    article.appendChild(p);
 
-    return section;
+    return article;
   };
   div.appendChild(quattroFormaggi());
 
